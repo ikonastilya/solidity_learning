21cea2b0449fef2284736bbd1b0bdc74e95c71a1
ikonastilya <bodyastalkerok@gmail.com>
2023-06-11 21:07:49 +0300

    fix: optimized imports

--- contracts/BohdanERC20Token.sol
+++ contracts/BohdanERC20Token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BohdanERC20Token is IERC20, Ownable {
     uint256 private _tokenPrice = 1;

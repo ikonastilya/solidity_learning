fc1e9a8d92c77f5d65cd52bc8ff5b02a4b635d0c
ikonastilya <bodyastalkerok@gmail.com>
2023-07-02 17:27:35 +0300

    feat: finished Voting contract

--- contracts/Votable.sol
+++ contracts/Votable.sol
@@ -1,37 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract VotingContract {
+import {BohdanERC20Token} from "./BohdanERC20Token.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract VotingContract is BohdanERC20Token, Ownable, ReentrancyGuard {
     uint256 public timeToVote = 1 weeks;
     uint256 public executionTime = 1 weeks;
     uint256 public amountToBurn;
 
     bool public actionExecuted;
 
+    uint256 public totalVotesCounted;
+
     // tests w/ typechain for time dependant things
 
-    struct Voter {
-        uint256 balance;
+    struct ProposedVote {
         uint256 vote;
-        bool voted;
-        address nextVoter;
+        uint256 power;
+        address next;
+        address prev;
     }
 
-    struct ProposedVotes {
-        uint256 vote;
-        uint256 powerOfVote;
-    } // todo: implement a second linked list that keeps the highest value as head 
-    // once voting is ended, set head price as the new price
-
-    mapping(address => Voter) private _voters;
+    mapping(address => ProposedVote) private _proposedPrices;
+    mapping(uint256 => address) private _isParticularPriceProposed;
+    mapping(address => address) private _voters;
     address private _head;
-    uint256 public totalSupply;
 
     uint256 public priceOption;
     uint256 public feePercentage;
     uint256 public votingEndTime;
 
-    mapping (uint256 => uint256) private _votesByPrice;
+    mapping(uint256 => uint256) private _votesByPrice;
 
     constructor(uint256 _feePercentage) {
         feePercentage = _feePercentage;
@@ -46,44 +47,86 @@ contract VotingContract {
         return balance > minimumBalance;
     }
 
-    function vote(uint256 _option) public {
+    function vote(
+        uint256 _option,
+        address prev,
+        address next
+    ) public {
+        address voterAddress = msg.sender;
+        uint256 voterBalance = balanceOf(voterAddress;)
+
         require(block.timestamp <= votingEndTime, "Voting period has ended");
         require(_option != priceOption, "Already voted for this option");
-        require(isAbleToVote(msg.sender), "Not enough balance to vote");
+        require(isAbleToVote(voterAddress), "Not enough balance to vote");
 
-        address voterAddress = msg.sender;
-        Voter storage voter = _voters[voterAddress];
-        require(voter.balance > 0, "You do not have any tokens");
-        require(!voter.voted, "Already voted");
+        require(voterBalance > 0, "You do not have any tokens");
+        require(_voters[voterAddress] == address(0), "Already voted");
 
-        voter.vote = _option;
-        voter.voted = true;
+        address index = _isParticularPriceProposed[_option];
 
-        if (voter.balance > _voters[_head].balance || _head == address(0)) {
-            voter.nextVoter = _head;
-            _head = voterAddress;
+        if (_proposedPrices[index].vote == 0) {
+            // if voter does not exist then we create one
+
+            totalVotesCounted++; // make +1 for loop so we know the length of mapping
+
+            _proposedPrices[voterAddress] = ProposedVote(
+                _option,
+                voterBalance,
+                prev,
+                next
+            );
+
+            sortNode(prev, next, voterAddress);
         } else {
-            _head = _insertVoter(_head, voterAddress);
+            // if it exists, we add the power to the price he wanted
+            _proposedPrices[index].power += voterBalance; 
+            sortNode(prev, next, index);
         }
     }
 
-    function _insertVoter(address _currentVoter, address _voterToInsert)
-        private
+    function givePricesList()
+        public
+        view
+        returns (mapping(address => ProposedVote))
     {
-        if (
-            _currentVoter == address(0) ||
-            _voters[_voterToInsert].balance > _voters[_currentVoter].balance
-        ) {
-            _voters[_voterToInsert].nextVoter = _currentVoter;
-        }
-
-        _voters[_currentVoter].nextVoter = _insertVoter(
-            _voters[_currentVoter].nextVoter,
-            _voterToInsert
-        );
+        return _proposedPrices;
     }
 
-    function burnFee() public {
+    function sortNode(
+        address prev,
+        address next,
+        address currentIndex
+    ) public {
+        if (
+            _proposedPrices[prev].power < _proposedPrices[currentIndex].power &&
+            _proposedPrices[next].power > _proposedPrices[currentIndex].power &&
+            _proposedPrices[prev].next == next
+        ) {
+            _proposedPrices[currentIndex].prev = prev;
+            _proposedPrices[currentIndex].next = next;
+
+            _proposedPrices[prev].next = currentIndex;
+            _proposedPrices[next].prev = currentIndex;
+
+            if (_proposedPrices[next].power != 0) {
+                if (
+                    _proposedPrices[currentIndex].power >
+                    _proposedPrices[_head].power
+                ) {
+                    _head = currentIndex;
+                }
+            }
+        } else {
+            revert("Wrong position");
+        }
+    }
+
+    function endVote() public onlyOwner {
+        require(block.timestamp > votingEndTime, "Voting not ended");
+        priceOption = _proposedPrices[_head].vote;
+    }
+
+    function burnFee() public onlyOwner {
         require(block.timestamp >= executionTime, "Only once a week");
         require(!actionExecuted, "Already executed");
 
@@ -93,61 +136,39 @@ contract VotingContract {
         actionExecuted = true;
     }
 
-    function buyTokens() public payable {
+    function buyTokens(address prev, address next, addressCurrentIndex) public payable nonReentrant {
         uint256 fee = (msg.value * feePercentage) / 100;
-        uint256 purchaseAmount = msg.value - fee; // todo: (msg.value - fee) * price
+        uint256 purchaseAmount = (msg.value - fee) * priceOption;
 
         require(purchaseAmount > 0, "Insufficient payment");
 
         address voterAddress = msg.sender;
         Voter storage voter = _voters[voterAddress];
-        voter.balance += purchaseAmount;
+
         totalSupply += purchaseAmount;
         amountToBurn += fee;
+        _balances[voterAddress] += purchaseAmount;
 
-        if (
-            voter.balance == purchaseAmount ||
-            _head == address(0) ||
-            _voters[_head].balance < voter.balance
-        ) {
-            voter.nextVoter = _head;
-            _head = voterAddress;
-        } else {
-            _insertVoter(_head, voterAddress);
+        if(_proposedPrices[votedAddress].vote !== 0) {
+            sortNode(prev, next, currentIndex)
         }
     }
 
-    function sellTokens(uint256 _amount) public {
+    function sellTokens(uint256 _amount, address prev, address next, address currentIndex) public nonReentrant {
         require(_voters[msg.sender].balance >= _amount, "Insufficient balance");
 
         uint256 fee = (_amount * feePercentage) / 100;
-        uint256 saleAmount = _amount - fee; // todo: (_amount - fee) / price;
+        uint256 saleAmount = (_amount - fee) * priceOption;
 
-        _voters[msg.sender].balance -= _amount;
+        _balances[msg.sender] -= _amount;
+
         totalSupply -= _amount;
         amountToBurn += fee;
 
-        payable(msg.sender).transfer(saleAmount * priceOption);
-    }
-
-    function calculateMostVotedPrice() public view returns (uint256) {
-        // Mapping to store the total votes for each price
-
-        address currentVoter = _head;
-
-        // Iterate through the linked list to count the votes for each price
-        while (currentVoter != address(0)) {
-            Voter memory voter = _voters[currentVoter];
-            uint256 voterBalance = voter.balance;
-            uint256 voterPrice = voter.vote;
-
-            _votesByPrice[voterPrice] += voterBalance;
-
-            currentVoter = voter.nextVoter;
+        if(_proposedPrices[votedAddress].vote !== 0) {
+            sortNode(prev, next, currentIndex)
         }
 
-        // todo: how to go through to calculate the mapping ????
-
+        payable(msg.sender).transfer(saleAmount);
     }
-
 }
